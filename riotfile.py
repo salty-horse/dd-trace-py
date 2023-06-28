194c33918abf030d81b6dade7ee14e233df90de8
Tahir H. Butt <tahir.butt@datadoghq.com>
2023-06-27 22:15:49 -0400

    chore: add hatch

--- riotfile.py
+++ riotfile.py
@@ -1566,6 +1566,7 @@ venv = Venv(
         Venv(
             name="asgi",
             pkgs={
+                "ddtrace": latest,
                 "pytest-asyncio": latest,
                 "httpx": latest,
                 "asgiref": ["~=3.0.0", "~=3.0", latest],
